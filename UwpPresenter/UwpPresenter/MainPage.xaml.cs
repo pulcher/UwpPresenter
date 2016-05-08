e9c33b77ca741e9a86943807bc0842c4bf805865
Harold Pulcher <pulcher@pulcher.biz>
2016-05-08 01:12:09 -0500

    got the images showing up on the page.  now to work on sizing the thing and working out the various layouts

--- UwpPresenter/UwpPresenter/MainPage.xaml.cs
+++ UwpPresenter/UwpPresenter/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +26,55 @@ namespace UwpPresenter
         public MainPage()
         {
             this.InitializeComponent();
+
+            var imageList = new List<string>();
+
+            GetFiles();
+
+            imageList.Add(@"Assets/IotHubTalk/iotHubTalk-png.001.png");
+            imageList.Add(@"Assets/IotHubTalk/iotHubTalk-png.002.png");
+
+            //flipView.Items.Add("Assets/IotHubTalk/iotHubTalk-png.001.png");
+            //flipView.Items.Add("Assets/IotHubTalk/iotHubTalk-png.002.png");
+
+            //foreach (var item in imageList)
+            //{
+            //    var uri = new Uri(string.Format(@"ms-appx:/{0}", item));
+            //    var img = new BitmapImage(uri);
+            //    flipView.Items.Add(img);
+            //}
+        }
+
+        private async System.Threading.Tasks.Task GetFiles()
+        {
+            var appInstalledFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var assets = await appInstalledFolder.GetFolderAsync("Assets");
+            var folder = await assets.GetFolderAsync("IotHubTalk");
+            var files = await folder.GetFilesAsync();
+
+            var imageList = new List<BitmapImage>();
+            foreach (var item in files)
+            {
+                var file = item.Name;
+                var path = item.Path;
+
+                //var uri = new Uri(string.Format(@"ms-appx:/Assests/IotHubTalk/{0}", file));
+                //var img = new BitmapImage(uri);
+
+                using(var fileStream = await item.OpenReadAsync()){
+                    var bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(fileStream);
+                    imageList.Add(bitmapImage);
+                    //flipView.Items.Add(bitmapImage);
+                }
+            }
+
+            flipView.ItemsSource = imageList;
+        }
+
+        private void flipView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
