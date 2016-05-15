ca856c57196fac83bb1aaf95cee69edc2008df45
Harold Pulcher <pulcher@pulcher.biz>
2016-05-15 12:30:26 -0500

    got the page finally doing what I think it should

--- UwpPresenter/UwpPresenter/ViewModels/UwpMain.cs
+++ UwpPresenter/UwpPresenter/ViewModels/UwpMain.cs
@@ -10,7 +10,9 @@ namespace UwpPresenter.ViewModels
     class UwpMain
     {
         private Observable<int> _currentPage = new Observable<int>();
+        private Observable<string> _currentDevice = new Observable<string>();
 
+        public string CurrentDevice { get { return _currentDevice; } set { _currentDevice.Value = value; } }
         public int SelectedPage { get { return _currentPage; } set { _currentPage.Value = value; } }
     }
 }
