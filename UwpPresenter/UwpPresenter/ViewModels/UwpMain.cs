c4aef795aa0abacb9e585a87c338c379b40cbef8
Harold Pulcher <pulcher@pulcher.biz>
2016-05-15 10:40:30 -0500

    stuff

---
+++ UwpPresenter/UwpPresenter/ViewModels/UwpMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assisticant.Fields;
+
+namespace UwpPresenter.ViewModels
+{
+    class UwpMain
+    {
+        private Observable<int> _currentPage = new Observable<int>();
+
+        public int SelectedPage { get { return _currentPage; } set { _currentPage.Value = value; } }
+    }
+}
---
+++ UwpPresenter/UwpPresenter/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assisticant.XAML;
+
+namespace UwpPresenter.ViewModels
+{
+    class ViewModelLocator : ViewModelLocatorBase
+    {
+        public object Main
+        {
+            get { return ViewModel(() => new UwpMain()); }
+        }
+    }
+}
